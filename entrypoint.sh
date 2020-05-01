052b9318f7de5b114f2ebef94f47e34fd9aa0e7b
Hubert Dąbrowski <h@dabrow.org>
2020-05-01 14:56:50 +0200

    Install ESLint locally
    ESLint 6.0 migration guide: "Plugins and shareable configs are no longer affected by ESLint's location"

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
-npm install -g ${INPUT_NPM_INSTALL_ARGS}
-eslint --version
+npm install ${INPUT_NPM_INSTALL_ARGS}
+node_modules/.bin/eslint --version
 
 result_file=$(mktemp)
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
   # Use jq and github-pr-review reporter to format result to include link to rule page.
-  (eslint -f="json" ${INPUT_ESLINT_FLAGS:-'.'}; echo $? > $result_file) \
+  (node_modules/.bin/eslint -f="json" ${INPUT_ESLINT_FLAGS:-'.'}; echo $? > $result_file) \
     | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
     | reviewdog -efm="%f:%l:%c:%m" \
         -name="${INPUT_TOOL_NAME}" \
@@ -24,7 +24,7 @@ if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
         ${INPUT_REVIEWDOG_FLAGS}
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  (eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'}; echo $? > $result_file) \
+  (node_modules/.bin/eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'}; echo $? > $result_file) \
     | reviewdog -f="eslint" \
         -name="${INPUT_TOOL_NAME}" \
         -reporter="${INPUT_REPORTER:-github-pr-check}" \
